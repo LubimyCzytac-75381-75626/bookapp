870c9509cdf154bf54ef0b03c09a2ea49f7b222d
Tymofii Salashnik <timofey.salashnik@gmail.com>
2026-06-05 21:42:39 +0200

    75626 - Dodałem kategorii książek

---
+++ src/main/java/pl/vistula/bookapp/jpa/model/Category.java
@@ -0,0 +1,38 @@
+package pl.vistula.bookapp.jpa.model;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.extern.jackson.Jacksonized;
+
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Accessors(chain=true)
+@Jacksonized
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "name")
+    @NotEmpty
+    String name;
+
+}
